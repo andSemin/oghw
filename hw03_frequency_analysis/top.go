69c75a0c3f32d636d048049f07f01e484b4080d4
andSemin <andsemin@gmail.com>
2023-05-17 13:00:23 +0300

    HW 3 completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,43 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type KeyVal struct {
+	word string
+	cnt  int
+}
+
+func Top10(s string) []string {
+	var r []string
+	words := strings.Fields(s)
+	cnt := make(map[string]int)
+	for _, k := range words {
+		cnt[k]++
+	}
+
+	kvs := make([]KeyVal, len(cnt))
+	i := 0
+	for k, v := range cnt {
+		kvs[i] = KeyVal{k, v}
+		i++
+	}
+
+	if len(cnt) < 1 {
+		return r
+	}
+
+	sort.Slice(kvs, func(i, j int) bool {
+		if kvs[i].cnt != kvs[j].cnt {
+			return kvs[i].cnt > kvs[j].cnt
+		}
+		return kvs[i].word < kvs[j].word
+	})
+
+	for i := 0; i < 10; i++ {
+		r = append(r, kvs[i].word)
+	}
+	return r
 }
