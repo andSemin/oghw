abe0fc0ff2175fcde8dc33ee0913fbab4bf71103
andSemin <andsemin@gmail.com>
2023-06-07 15:46:55 +0300

    Hw05 parallel execution (#4)
    * Done home work 5
    
    * Fix race cond
    
    * Fix delete .sync
    
    * Delete extra test

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,67 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+
+var ErrNotEnoughGoroutines = errors.New("not enough goroutines")
+
+var ErrNegNumberOfAllowedErrors = errors.New("negative number of allowed errors")
 
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if n < 1 {
+		return ErrNotEnoughGoroutines
+	}
+
+	if m < 0 {
+		return ErrNegNumberOfAllowedErrors
+	}
+
+	if m > len(tasks) {
+		m = len(tasks)
+	}
+
+	errLimit := int64(m)
+	if m == 0 {
+		errLimit += int64(len(tasks)) + 1
+	}
+
+	ch := make(chan Task)
+	var errCnt int64
+	var wg sync.WaitGroup
+
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			for t := range ch {
+				tt := t()
+				if tt != nil {
+					atomic.AddInt64(&errCnt, 1)
+				}
+			}
+		}()
+	}
+
+	for _, t := range tasks {
+		if atomic.LoadInt64(&errCnt) >= errLimit {
+			break
+		}
+		ch <- t
+	}
+
+	close(ch)
+	wg.Wait()
+
+	if atomic.LoadInt64(&errCnt) >= errLimit {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
